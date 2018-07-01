d067d99245df025412ea2c8dac076e61a4fe7678
James Dillard <jamesedwarddillard@gmail.com>
2018-07-01 08:54:35 -0700

    added parsing;

--- congress.py
+++ congress.py
@@ -1,15 +1,16 @@
 import urllib, urllib2, requests
 from secrets import API_KEY
 
-# url = 'http://opensecrets.org/api/?method=candSummary&output=json&apikey=989a4fa91e411e9efe316ef266f3fe18&cycle=2014&cid=N00007360'
+cid ='N00007360'
 
-def candSummary(apikey,cid, cycle='2018', output ='json'):
+def candidate_summary(apikey,cid, cycle='2018', output ='json'):
 	url = 'http://opensecrets.org/api/?method=candSummary&output=%s&apikey=%s&cycle=%s&cid=%s' % \
 		(output,apikey,cycle, cid )
 	response = requests.get(url)
 	return response.json()
 
 def candidate_details(response):
-	try:
-		response["response"]
+	details =response['response']['summary']['@attributes']
+	return details
 
+print candidate_details(candidate_summary(API_KEY, cid))
