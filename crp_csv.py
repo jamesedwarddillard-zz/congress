7a5e2aa9ec988443a66603a4ba882cde93fd336a
James Dillard <jamesedwarddillard@gmail.com>
2018-07-04 00:51:13 -0700

    updates to support csv creation

--- crp_csv.py
+++ crp_csv.py
@@ -1,12 +1,31 @@
 import csv
+from crpapi import candidate_summary, candidate_details
+from secrets import API_KEY
 
-def candidate_csv(csv_name):
+def candidate_csv(csv_name): # will only work with the CRP formatted CSV
 	with open(csv_name) as file:
-		filereader = csv.reader(file)
-		data = [row for row in filereader]
+		reader = csv.DictReader(file)
+		data = [row for row in reader]
 		return data
 
-# need to pop header off and then start a process to organize the data
-# you'll then call for the cid and then merge it with the existing data
+def enriched_data(candidate_data_dict):
+	cid = candidate_data_dict['CID']
+	crp_online_data = candidate_details(candidate_summary(apikey = API_KEY, cid = cid))
+	candidate_data= {}
+	candidate_data['cid'] = cid # data from the original csv
+	candidate_data['name'] = candidate_data_dict['CRPName']
+	candidate_data['district'] = candidate_data_dict['DistIDRunFor']
+	candidate_data['party'] = candidate_data_dict['Party']
+	candidate_data['spent'] = crp_online_data['spent'] # data pulled from the api
+	candidate_data['total'] = crp_online_data['total']
+	candidate_data['cash on hand'] = crp_online_data['cash_on_hand']
+	candidate_data['debt'] = crp_online_data['debt']
+	candidate_data['updated date'] = crp_online_data['last_updated']
+	return candidate_data
 
-print candidate_csv('test_candidates.csv')
+def new_csv(old_csv):
+	data = candidate_csv(old_csv)
+	# have to have a way to skip the blanks here
+	for row in
+
+print enriched_data(candidate_csv('test_candidates.csv')[0]).keys()
--- crpapi.py
+++ crpapi.py
@@ -4,11 +4,13 @@ from secrets import API_KEY
 # These functions work with the Center for Responsible Politics API to isolate data on a per candidate basis
 
 def candidate_summary(apikey,cid, cycle='2018', output ='json'):
+	# calls the CRP API for a specific method
 	url = 'http://opensecrets.org/api/?method=candSummary&output=%s&apikey=%s&cycle=%s&cid=%s' % \
 		(output,apikey,cycle, cid )
 	response = requests.get(url)
 	return response.json()
 
 def candidate_details(response):
+	# parses the response for the candidate summary method
 	details =response['response']['summary']['@attributes']
 	return details
